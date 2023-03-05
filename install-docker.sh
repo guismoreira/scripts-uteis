970b0e7161f4b2e108dcc3d71bed44c8fd9f6aa6
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-03-05 14:17:27 -0300

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo usermod -aG docker $USER
+
+# as linhas abaixo serão úteis no wsl
+# nano ~/.bashrc
+# alias docker-start="sudo service docker start"
+# docker-start
