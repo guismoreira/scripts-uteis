4437903f37a870f9dfa73acfb183e1e29fbc7973
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-03-05 17:01:35 -0300

    Create install-jdk-maven.sh

---
+++ install-jdk-maven.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
+
+wget wget https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.tar.gz
+
+sudo tar xvf apache-maven-3.9.0-bin.tar.gz -C /opt
+
+sudo ln -s /opt/apache-maven-3.9.0 /opt/maven
+
+echo 'export M2_HOME=/opt/maven export PATH=${M2_HOME}/bin:${PATH}' > /etc/profile.d/maven.sh
+
+sudo chmod +x /etc/profile.d/maven.sh
+
+source /etc/profile.d/maven.sh
+
