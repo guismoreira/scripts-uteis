84cbf8ed6dbd27c005f48880b0e7bc91fb1a4bc6
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-03-06 10:40:30 -0300

    Update install-jdk-maven.sh

--- install-jdk-maven.sh
+++ install-jdk-maven.sh
@@ -8,8 +8,8 @@ sudo tar xvf apache-maven-3.9.0-bin.tar.gz -C /opt
 
 sudo ln -s /opt/apache-maven-3.9.0 /opt/maven
 
-echo 'export M2_HOME=/opt/maven' > /etc/profile.d/maven.sh
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh
+sudo echo 'export M2_HOME=/opt/maven' > /etc/profile.d/maven.sh
+sudo echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh
 
 sudo chmod +x /etc/profile.d/maven.sh
 
