fd1ec177c909fd7fd9f38f91a7da3e70a8598aec
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-03-13 15:31:36 -0300

    Create install-springcli.sh

---
+++ install-springcli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install unzip zip -y
+sudo apt install curl -y
+curl -s https://get.sdkman.io | bash
+source "/root/.sdkman/bin/sdkman-init.sh"
+sdk install springboot
+
+echo 'alias spring-api="spring init --build=maven --java-version=17 --dependencies=web,h2,jdbc,lombok,data-jpa"' >> ~/.bashrc
