2ea8f9fdb5dfd9b40a66f6cef0413881b7d39182
Guilherme Moreira <guilhermemoreira55@gmail.com>
2023-03-13 15:26:01 -0300

    Update install-jdk-maven.sh

--- install-jdk-maven.sh
+++ install-jdk-maven.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-
-sudo apt install openjdk-17-jre-headless -y
-
-wget wget https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.tar.gz
-
-sudo tar xvf apache-maven-3.9.0-bin.tar.gz -C /opt
-
-sudo ln -s /opt/apache-maven-3.9.0 /opt/maven
-
-sudo su
-echo 'export M2_HOME=/opt/maven' > /etc/profile.d/maven.sh
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh
-exit
-
-sudo chmod +x /etc/profile.d/maven.sh
-
-source /etc/profile.d/maven.sh
-
+sudo apt install unzip zip -y
+sudo apt install curl -y
+curl -s https://get.sdkman.io | bash
+source "/root/.sdkman/bin/sdkman-init.sh"
+sdk install java
+sdk install maven
